5f7e9d13f018ba5e88d2ccde2e17cfd8c036c970
Reyes Ruiz <reyes@digitalruiz.com>
2023-05-15 17:26:30 -0700

    Trying await

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -28,17 +28,36 @@ export default function HomePage() {
   const isRefetching = false;
   const [showResourcePicker, setShowResourcePicker] = useState(false);
   const appBridge = useAppBridge();
-  const getURL = (url, headers) => {
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getData = async (url, headers) => {
     console.log("URL is: %s", url)
-      fetch(url, {
-    method: 'get',
-    headers: headers,
-      }).then((res) => res.text()).then((data) => {
-        console.log(data);
-      }).catch((err) => {
-        console.log(err.message);
-      });
-    //return
+    try {
+      const response = await fetch(
+        url, {
+          method: 'get',
+          headers: headers,
+        }
+      );
+      if (!response.ok) {
+        throw new Error(
+          'This is an HTTP error: The status is ${response.status}'
+        );
+      }
+      let actualData = await response.json();
+      setData(actualData);
+      setError(null);
+    }
+    catch (err) {
+      setError(err.message);
+      setData(null);
+    }
+    finally {
+      setLoading(false);
+    }
   }
 
   const handleProductChange = useCallback(({ selection }) => {
@@ -53,7 +72,7 @@ export default function HomePage() {
       const shopify_django_apis_token = import.meta.env.VITE_SHOPIFY_DJANGO_APIS_TOKEN;
       const headers = new Headers({'Authorization': 'Token ' + shopify_django_apis_token});
       console.log(url);
-      getURL(url, headers);
+      getData(url, headers);
     }
 
     setShowResourcePicker(false);
